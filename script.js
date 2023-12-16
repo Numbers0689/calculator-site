58ed87225d210d43e25b4175b2fc191fd7d5d126
Numbers0689 <firewatchsai@gmail.com>
2023-12-16 13:58:36 +0530

    Setup files and create basic opeartion fns

---
+++ script.js
@@ -0,0 +1,24 @@
+function add(a, b) {
+    return a+b;
+}
+
+function subtract(a, b) {
+    return a-b;
+}
+
+function multiply(a, b) {
+    return a*b;
+}
+
+function divide(a, b) {
+    return a/b;
+}
+
+console.log(add(11, 14));
+console.log(add(11.5, 13.5));
+console.log(subtract(11, 14));
+console.log(subtract(11.5, 13.5));
+console.log(multiply(11, 14));
+console.log(multiply(11.5, 13.5));
+console.log(divide(11, 14));
+console.log(divide(11.5, 13.5));
