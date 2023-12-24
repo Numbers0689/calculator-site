50bf7e92ad34a408a57168737ade58e4c3d28899
Numbers0689 <firewatchsai@gmail.com>
2023-12-24 19:01:21 +0530

    Add button query selctors

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 let numA, numB, operator;
+const display = document.querySelector('#display');
+const one = document.querySelector('.num.1');
+
+one.addEventListener('click', () => {
+    display.textContent = "1";
+    console.log("works");
+})
+
+display.textContent = "455435";
 
 function operate(a, b, operator) {
     switch (operator) {
@@ -35,11 +44,11 @@ function divide(a, b) {
     return a/b;
 }
 
-console.log(add(11, 14));
-console.log(add(11.5, 13.5));
-console.log(subtract(11, 14));
-console.log(subtract(11.5, 13.5));
-console.log(multiply(11, 14));
-console.log(multiply(11.5, 13.5));
-console.log(divide(11, 14));
-console.log(divide(11.5, 13.5));
+// console.log(add(11, 14));
+// console.log(add(11.5, 13.5));
+// console.log(subtract(11, 14));
+// console.log(subtract(11.5, 13.5));
+// console.log(multiply(11, 14));
+// console.log(multiply(11.5, 13.5));
+// console.log(divide(11, 14));
+// console.log(divide(11.5, 13.5));
